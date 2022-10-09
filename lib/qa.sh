c41fb28fba3b16b8c08a9e063a960a7c332c703e
Rinat Abdullin <rinat.abdullin@gmail.com>
2022-10-09 15:24:16 +0200

    Disable go

--- lib/qa.sh
+++ lib/qa.sh
@@ -8,4 +8,4 @@ BRANCH=$3
   #git --work-tree=$TARGET --git-dir=$(pwd) checkout -f $BRANCH && cd $TARGET && lib/qa.sh >/dev/null 2>&1 &
 echo start
 git --work-tree=$TARGET --git-dir=$DIR checkout -f $BRANCH
-cd $TARGET && /usr/local/go/bin/go run *.go && lib/send.py
+#cd $TARGET && /usr/local/go/bin/go run *.go && lib/send.py
