6c088fffe85b45d31406ef6ee2d16c66234242b9
Rinat Abdullin <rinat.abdullin@gmail.com>
2022-10-09 15:22:55 +0200

    Trying to fix it

--- lib/qa.sh
+++ lib/qa.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -eu -o pipefail -o xtrace
 TARGET=$1
-GIT=$2
+DIR=$2
 BRANCH=$3
 
 
   #git --work-tree=$TARGET --git-dir=$(pwd) checkout -f $BRANCH && cd $TARGET && lib/qa.sh >/dev/null 2>&1 &
 
-git --work-tree=$TARGET --git-dir=$GIT checkout -f $BRANCH
+git --work-tree=$TARGET --git-dir=$DIR checkout -f $BRANCH
 cd $TARGET && /usr/local/go/bin/go run *.go && lib/send.py
