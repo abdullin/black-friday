213f9fb6d593fccdbaac25e4bbecb72511e12d40
Rinat Abdullin <rinat.abdullin@gmail.com>
2022-10-09 15:33:15 +0200

    tweak qa

--- lib/qa.sh
+++ lib/qa.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 set -eu -o pipefail -o xtrace
-TARGET=$1
-DIR=$2
-BRANCH=$3
-
-
-  #git --work-tree=$TARGET --git-dir=$(pwd) checkout -f $BRANCH && cd $TARGET && lib/qa.sh >/dev/null 2>&1 &
-echo start
-git --work-tree=$TARGET --git-dir=$DIR checkout -f $BRANCH
-#cd $TARGET && /usr/local/go/bin/go run *.go && lib/send.py
+go run *.go && lib/send.py
