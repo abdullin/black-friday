2d8b779361b5908739c2afb2b2b8cabab92e7c69
Rinat Abdullin <rinat.abdullin@gmail.com>
2022-10-09 15:19:31 +0200

    Simplify qa

--- lib/qa.sh
+++ lib/qa.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# git --work-tree=$TARGET --git-dir=$DIR checkout -f $BRANCH >> /dev/null
-/usr/local/go/bin/go run *.go && lib/send.py
+TARGET=$1
+GIT=$2
+BRANCH=$3
+
+
+  #git --work-tree=$TARGET --git-dir=$(pwd) checkout -f $BRANCH && cd $TARGET && lib/qa.sh >/dev/null 2>&1 &
+
+git --work-tree=$TARGET --git-dir=$GIT checkout -f $BRANCH
+cd $TARGET && /usr/local/go/bin/go run *.go && lib/send.py
