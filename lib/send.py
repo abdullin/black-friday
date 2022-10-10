632c15c290407c0f8c43f82488f1291a626f2341
Rinat Abdullin <rinat.abdullin@gmail.com>
2022-10-10 17:09:29 +0200

    Print telegram failures

--- lib/send.py
+++ lib/send.py
@@ -17,6 +17,10 @@ def send(text):
     safe_string = urllib.parse.quote_plus(text)
 
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage?chat_id={chat_id}&text={safe_string}&parse_mode=MarkdownV2"
-    requests.get(url).json() # this sends the message
+    response = requests.get(url)
+
+
+    if not response.ok:
+        print(response.json())
 
 send(data)
