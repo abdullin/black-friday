2f6801c52f5a417fe9f7273f2d5dc0ab54ae674e
Rinat Abdullin <rinat.abdullin@gmail.com>
2022-10-25 11:26:40 +0200

    Small cleanups

--- inventory/schema.go
+++ inventory/schema.go
@@ -7,12 +7,7 @@ import _ "embed"
 //go:embed schema.sql
 var schema string
 
-type Store struct {
-	db *sql.DB
-}
-
 func CreateSchema(db *sql.DB) error {
 	_, err := db.Exec(schema)
 	return err
-
 }
