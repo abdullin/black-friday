3b430af3c20446397c8bac8555d0ff8b51602072
Rinat Abdullin <rinat.abdullin@gmail.com>
2022-10-27 13:19:41 +0200

    Push api in products to dot import

--- inventory/features/products/add.go
+++ inventory/features/products/add.go
@@ -2,11 +2,11 @@ package products
 
 import (
 	"black-friday/fail"
-	"black-friday/inventory/api"
+	. "black-friday/inventory/api"
 	"black-friday/inventory/app"
 )
 
-func Add(ctx *app.Context, req *api.AddProductsReq) (r *api.AddProductsResp, err error) {
+func Add(ctx *app.Context, req *AddProductsReq) (r *AddProductsResp, err error) {
 
 	id := ctx.GetSeq("Products")
 
@@ -14,7 +14,7 @@ func Add(ctx *app.Context, req *api.AddProductsReq) (r *api.AddProductsResp, err
 	for i, sku := range req.Skus {
 
 		id += 1
-		e := &api.ProductAdded{
+		e := &ProductAdded{
 			Id:  id,
 			Sku: sku,
 		}
@@ -23,13 +23,13 @@ func Add(ctx *app.Context, req *api.AddProductsReq) (r *api.AddProductsResp, err
 		switch f {
 		case fail.None:
 		case fail.ConstraintUnique:
-			return nil, api.ErrDuplicateName
+			return nil, ErrDuplicateName
 		default:
-			return nil, api.ErrInternal(err, f)
+			return nil, ErrInternal(err, f)
 		}
 
 		results[i] = id
 	}
 
-	return &api.AddProductsResp{Ids: results}, nil
+	return &AddProductsResp{Ids: results}, nil
 }
