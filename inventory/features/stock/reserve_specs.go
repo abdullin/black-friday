1b1483af77ee05fdd63bfb427d02ec8a6191a0be
Rinat Abdullin <rinat.abdullin@gmail.com>
2022-12-02 22:36:59 +0100

    Add more specs

--- inventory/features/stock/reserve_specs.go
+++ inventory/features/stock/reserve_specs.go
@@ -32,6 +32,167 @@ func init() {
 	})
 
 	Define(&Spec{
+		Name: "reserve sale in a specific location",
+		Given: []proto.Message{
+			&ProductAdded{Id: 1, Sku: "GPU"},
+			&LocationAdded{Id: 1, Name: "Shelf"},
+			&InventoryUpdated{Location: 1, Product: 1, OnHandChange: 10, OnHand: 10},
+		},
+		When: &ReserveReq{
+			Reservation: "sale",
+			Location:    1,
+			Items: []*ReserveReq_Item{
+				{Sku: "GPU", Quantity: 10},
+			},
+		},
+		ThenResponse: &ReserveResp{Reservation: 1},
+		ThenEvents: []proto.Message{
+			&Reserved{
+				Reservation: 1,
+				Code:        "sale",
+				Items: []*Reserved_Item{
+					{Product: 1, Quantity: 10, Location: 1},
+				},
+			},
+		},
+	})
+
+	Define(&Spec{
+		Name: "reserve sale in a specific location that doesn't have quantity",
+		Given: []proto.Message{
+			&ProductAdded{Id: 1, Sku: "GPU"},
+			&LocationAdded{Id: 1, Name: "Shelf"},
+			&LocationAdded{Id: 2, Name: "Empty"},
+			&InventoryUpdated{Location: 1, Product: 1, OnHandChange: 10, OnHand: 10},
+		},
+		When: &ReserveReq{
+			Reservation: "sale",
+			Location:    2,
+			Items: []*ReserveReq_Item{
+				{Sku: "GPU", Quantity: 10},
+			},
+		},
+		ThenError: ErrNotEnough,
+	})
+
+	Define(&Spec{
+		Name: "reserve in a location that contains enough inside",
+		Given: []proto.Message{
+			&ProductAdded{Id: 1, Sku: "GPU"},
+			&LocationAdded{Id: 1, Name: "Container"},
+			&LocationAdded{Id: 2, Name: "Box", Parent: 1},
+			&InventoryUpdated{Location: 2, Product: 1, OnHandChange: 10, OnHand: 10},
+		},
+		When: &ReserveReq{
+			Reservation: "sale",
+			Location:    1,
+			Items: []*ReserveReq_Item{
+				{Sku: "GPU", Quantity: 10},
+			},
+		},
+		ThenEvents: []proto.Message{
+			&Reserved{
+				Reservation: 1,
+				Code:        "sale",
+				Items: []*Reserved_Item{
+					{Product: 1, Quantity: 10, Location: 1},
+				},
+			},
+		},
+	})
+
+	Define(&Spec{
+		Name: "reserve in a location that doesn't have enough inside",
+		Given: []proto.Message{
+			&ProductAdded{Id: 1, Sku: "GPU"},
+			&LocationAdded{Id: 1, Name: "Container"},
+			&LocationAdded{Id: 2, Name: "Box", Parent: 1},
+			&InventoryUpdated{Location: 2, Product: 1, OnHandChange: 10, OnHand: 10},
+		},
+		When: &ReserveReq{
+			Reservation: "sale",
+			Location:    1,
+			Items: []*ReserveReq_Item{
+				{Sku: "GPU", Quantity: 11},
+			},
+		},
+		ThenError: ErrNotEnough,
+	})
+
+	Define(&Spec{
+		Name: "reserve box while container has a reservation on top of it (enough)",
+		Given: []proto.Message{
+			&ProductAdded{Id: 1, Sku: "GPU"},
+			&LocationAdded{Id: 1, Name: "Container"},
+			&LocationAdded{Id: 2, Name: "Box", Parent: 1},
+			&InventoryUpdated{Location: 2, Product: 1, OnHandChange: 10, OnHand: 10},
+			&Reserved{
+				Reservation: 1,
+				Code:        "sale0",
+				Items: []*Reserved_Item{
+					{
+						Product:  1,
+						Quantity: 5,
+						Location: 1,
+					},
+				},
+			},
+		},
+		When: &ReserveReq{
+			Reservation: "sale2",
+			Location:    2,
+			Items: []*ReserveReq_Item{
+				{Sku: "GPU", Quantity: 4},
+			},
+		},
+		ThenResponse: &ReserveResp{
+			Reservation: 2,
+		},
+		ThenEvents: []proto.Message{
+			&Reserved{
+				Reservation: 1,
+				Code:        "sale2",
+				Items: []*Reserved_Item{
+					{
+						Product:  1,
+						Quantity: 4,
+						Location: 2,
+					},
+				},
+			},
+		},
+	})
+
+	Define(&Spec{
+		Name: "reserve box while container has a reservation on top of it (not enough)",
+		Given: []proto.Message{
+			&ProductAdded{Id: 1, Sku: "GPU"},
+			&LocationAdded{Id: 1, Name: "Container"},
+			&LocationAdded{Id: 2, Name: "Box", Parent: 1},
+			&InventoryUpdated{Location: 2, Product: 1, OnHandChange: 10, OnHand: 10},
+			&Reserved{
+				Reservation: 1,
+				Code:        "sale0",
+				Items: []*Reserved_Item{
+					{
+						Product:  1,
+						Quantity: 5,
+						Location: 1,
+					},
+				},
+			},
+		},
+		When: &ReserveReq{
+			Reservation: "sale2",
+			Location:    2,
+			Items: []*ReserveReq_Item{
+				{Sku: "GPU", Quantity: 6},
+			},
+		},
+		ThenError: ErrNotEnough,
+	})
+
+	Define(&Spec{
 		Name: "reserve non-existent sku",
 		When: &ReserveReq{
 			Reservation: "test",
